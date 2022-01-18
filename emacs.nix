ace3e6a20293cf18cb021d6f4a12dc6fe8c0e51d
Anthony Cowley <acowley@gmail.com>
2022-01-17 19:33:15 -0500

    Add ledger-mode

--- emacs.nix
+++ emacs.nix
@@ -450,6 +450,7 @@ in {
     ligature
 
     cuda-mode
+    ledger-mode
   ];
   myemacsPkgs = (self.emacsPackagesFor self.emacsGit).overrideScope' self.myEmacsPackageOverrides;
   myemacs = ((self.emacsPackagesFor self.emacsGit).overrideScope' self.myEmacsPackageOverrides).emacsWithPackages self.myEmacsPackages;
